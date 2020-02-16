f0cbe7207e2d7e5f391cf02b749d0d1831e50f91
mattyg915 <matt.gercz@icloud.com>
2020-02-15 18:31:03 -0600

    users initalize done

--- zendesk-search-server/src/modules/users/User.ts
+++ zendesk-search-server/src/modules/users/User.ts
@@ -1,4 +1,5 @@
 import users from "../../data/users.json"
+import { deflateRaw } from "zlib";
 
 /**
  * User class contains data and functions related to an
@@ -10,12 +11,22 @@ import users from "../../data/users.json"
     url!: string;
     external_id!: string;
     name!: string;
-    alias!: string;
+    alias!: string | undefined;
     created_at!: Date;
-    
+    active!: boolean;
+    verified!: boolean | undefined;
+    shared!: boolean;
+    locale!: string | undefined;
+    timezone!: string | undefined;
+    last_login_at!: Date;
+    email!: string | undefined;
+    phone!: string;
+    signature!: string;
+    organization_id!: number | undefined;
     tags!: Array<string>;
+    suspended!: boolean;
+    role!: string;
     
-
     constructor(userObj: user) {
         Object.assign(this, userObj);
     }
@@ -29,15 +40,30 @@ import users from "../../data/users.json"
 
         for (let data of users) {
             // date strings in the data contain a space that can't be parsed properly
-            data["created_at"] = data["created_at"].replace(/\s/g, '');
+            data.created_at = data.created_at.replace(/\s/g, '');
+            data.last_login_at = data.last_login_at.replace(/\s/g, '');
 
             newUsers.push(
                 new user({
-                    _id: data["_id"],
-                    url: data["url"],
-                    external_id: data["external_id"],
-                    created_at: new Date(data["created_at"]),
-                    
+                    _id: data._id,
+                    url: data.url,
+                    external_id: data.external_id,
+                    name: data.name,
+                    alias: data.alias,
+                    created_at: new Date(data.created_at),
+                    active: data.active,
+                    verified: data.verified,
+                    shared : data.shared,
+                    locale : data.locale,
+                    timezone : data.timezone,
+                    last_login_at : new Date(data.last_login_at),
+                    email : data.email,
+                    phone: data.phone,
+                    signature : data.signature,
+                    organization_id : data.organization_id,
+                    tags : data.tags,
+                    suspended : data.suspended,
+                    role : data.role
                 })
             );
         }
