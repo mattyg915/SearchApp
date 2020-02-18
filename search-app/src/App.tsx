e2ddc10fa0e67db3692b25260a1c7d8ed8c4d046
mattyg915 <matt.gercz@icloud.com>
2020-02-17 20:46:44 -0600

    get router working

--- search-app/src/App.tsx
+++ search-app/src/App.tsx
@@ -1,37 +1,51 @@
 import React, { Component } from "react";
+import { Link, Router, Route, NavLink } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import SearchOrganization from "./interface/search/organization/searchOrganization";
 import "./app.scss";
 
 interface searchAppProps {}
 
 interface searchAppState {}
 
+const history = createBrowserHistory();
+
 class App extends Component<searchAppProps, searchAppState> {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h1>Welcome to TicketSearch!</h1>
-        </div>
-        <div className="options">
-          <div className="options-text">Select a search option</div>
-          <div className="option-select">
-            <button className="option-button" onClick={this.searchOrgs}>
-              Search Organizations
-            </button>
-            <button className="option-button" onClick={this.searchTickets}>
-              Search Tickets
-            </button>
-            <button className="option-button" onClick={this.searchUsers}>
-              Search Users
-            </button>
+      <Router history={history}>
+        <Route
+          component={SearchOrganization}
+          path="/search-organization"
+          exact
+        />
+        <div className="App">
+          <div className="App-header">
+            <h1>Welcome to TicketSearch!</h1>
+          </div>
+          <div className="options">
+            <div className="options-text">Select a search option</div>
+            <div className="option-select">
+              <NavLink className="option-button" to="/search-organization">
+                Search Organizations
+              </NavLink>
+              <button className="option-button" onClick={this.searchTickets}>
+                Search Tickets
+              </button>
+              <button className="option-button" onClick={this.searchUsers}>
+                Search Users
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      </Router>
     );
   }
 
   searchOrgs = () => {};
+
   searchTickets = () => {};
+
   searchUsers = () => {};
 }
 
